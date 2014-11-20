04ddbeccfe20c119cbdcd33a08107424ee7e8c27
Denis Nurboja <denis.nurboja@drtechno.ba>
2014-11-20 14:54:06 +0000

    search

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
 	validates :first_name, :last_name, :email, :phone, :adress, :pobox, 
 			  :city, presence: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-
+  private
 	def self.search(query)
-  		where("first_name like ?", "%#{query}%")
+    where("first_name like ? or last_name like ?", "%#{query}%", "%#{query}%") 
 	end
 end
 
